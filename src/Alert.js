b039d7f19a272b8f075b1c32a40ad4e028320fee
Prakruti Thakur <prakriti1921@gmail.com>
2023-01-05 03:45:28 +0530

    tic tac toe game

---
+++ src/Alert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Alert = (props) => {
+    
+    return (
+        <div style={{height:'50px'}}>
+        <div class="alert alert-success" role="alert">
+  This is a success alert—check it out!
+</div>
+    </div>
+    );
+}
+
+
+
+export default Alert;
